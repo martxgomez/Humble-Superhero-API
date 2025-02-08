4d048820af5488983ed6882f62bd11d12875d778
martxgomez <martagomezmartinez95@gmail.com>
2025-02-08 23:01:35 +0100

    Get and post of superheroesDB

--- server/app.js
+++ server/app.js
@@ -1,22 +1,52 @@
 //IMPORTS
 const express = require("express");
 const Superhero = require("./Superhero)");
+const PORT = 3000;
 
 const app = express();
 
 //ARRAY OF SUPERHEROES
 const superheroesDB = [
-    new Superhero("Superman", "Flight", 9),
-    new Superhero("Batman", "Martial Arts", 8),
-    new Superhero("Wonder Woman", "Super Strength", 10),
-    new Superhero("Spiderman", "Wall-crawling", 7),
-    new Superhero("Ironman", "Genius", 6),
-    new Superhero("Hulk", "Super Strength", 10),
-    new Superhero("Flash", "Speed", 8),
-    new Superhero("Aquaman", "Aquatic Abilities", 7),
-    new Superhero("Black Panther", "Enhanced Senses", 9),
-    new Superhero("Thor", "Thunder Power", 8)
-  ];
+  new Superhero("Superman", "Flight", 9),
+  new Superhero("Batman", "Martial Arts", 8),
+  new Superhero("Wonder Woman", "Super Strength", 10),
+  new Superhero("Spiderman", "Wall-crawling", 7),
+  new Superhero("Ironman", "Genius", 6),
+  new Superhero("Hulk", "Super Strength", 10),
+  new Superhero("Flash", "Speed", 8),
+  new Superhero("Aquaman", "Aquatic Abilities", 7),
+  new Superhero("Black Panther", "Enhanced Senses", 9),
+  new Superhero("Thor", "Thunder Power", 8),
+];
 
-  
+//ROUTES
 
+app.get("/superheroes", (req, res) => {
+  //Sort by humility score in descending order
+  const sortedSuperheroes = superheroesDB.sort(
+    (a, b) => b.humilityScore - a.humilityScore
+  );
+  res.json(sortedSuperheroes);
+});
+
+app.post("/superheroes", (req, res) => {
+  const { name, superpower, humilityScore } = req.body;
+
+  //Condition if name, superpower or humility score fields do not exist
+  if (!name || !superpower || !humilityScore) {
+    res.status(500).json({ error: "Failed to create the Superhero" });
+  } else {
+    const newSuperhero = new Superhero(name, superpower, humilityScore);
+    superheroesDB.push(newSuperhero);
+    res.status(201).json({
+      message: "Superhero created sucessfully",
+      superhero: newSuperhero,
+    });
+  }
+
+});
+
+//START SERVER
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
