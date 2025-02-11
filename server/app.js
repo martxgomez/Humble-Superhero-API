bf11f31ba3e83e422f3ddff19e16beb35a4143ff
martxgomez <martagomezmartinez95@gmail.com>
2025-02-11 22:36:23 +0100

    error handling

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const Superhero = require("./Superhero.js");
 const PORT = 3000;
+const errorHandler = require("./error-handling/index.js");
 
 const app = express();
 app.use(express.json());
@@ -10,22 +11,22 @@ app.use(cors());
 
 //ARRAY OF SUPERHEROES
 const superheroesDB = [
-  new Superhero("Superman", "Flight", 9),
-  new Superhero("Batman", "Martial Arts", 8),
-  new Superhero("Wonder Woman", "Super Strength", 10),
-  new Superhero("Spiderman", "Wall-crawling", 7),
-  new Superhero("Ironman", "Genius", 6),
-  new Superhero("Hulk", "Super Strength", 10),
-  new Superhero("Flash", "Speed", 8),
-  new Superhero("Aquaman", "Aquatic Abilities", 7),
-  new Superhero("Black Panther", "Enhanced Senses", 9),
-  new Superhero("Thor", "Thunder Power", 8),
+  new Superhero("Superman", "Flight", 9, 1),
+  new Superhero("Batman", "Martial Arts", 8, 2),
+  new Superhero("Wonder Woman", "Super Strength", 10, 3),
+  new Superhero("Spiderman", "Wall-crawling", 7, 4),
+  new Superhero("Ironman", "Genius", 6, 5),
+  new Superhero("Hulk", "Super Strength", 10, 6),
+  new Superhero("Flash", "Speed", 8, 7),
+  new Superhero("Aquaman", "Aquatic Abilities", 7, 8),
+  new Superhero("Black Panther", "Enhanced Senses", 9, 9),
+  new Superhero("Thor", "Thunder Power", 8, 10),
 ];
 
 //ROUTES
 app.get("/superheroes", (req, res) => {
   //Sort by humility score in descending order
-  const sortedSuperheroes = superheroesDB.sort(
+  const sortedSuperheroes = superheroesDB.slice().sort(
     (a, b) => b.humilityScore - a.humilityScore
   );
   res.json(sortedSuperheroes);
@@ -36,9 +37,14 @@ app.post("/superheroes", (req, res) => {
 
   //Condition if name, superpower or humility score fields do not exist
   if (!name || !superpower || !humilityScore) {
-    res.status(500).json({ error: "Failed to create the Superhero" });
+    res.status(400).json({ error: "Failed to create the Superhero" });
   } else {
-    const newSuperhero = new Superhero(name, superpower, humilityScore);
+    const newSuperhero = new Superhero(
+      name,
+      superpower,
+      humilityScore,
+      superheroesDB.length+1
+    );
 
     superheroesDB.push(newSuperhero);
     res.status(201).json({
@@ -47,6 +53,11 @@ app.post("/superheroes", (req, res) => {
     });
   }
 });
+app.get("/error", (req, res, next) => {
+  next(new Error("Este es un error forzado"));
+});
+
+errorHandler(app);
 
 //START SERVER
 app.listen(PORT, () => {
--- server/superhero.js
+++ server/superhero.js
@@ -1,23 +1,26 @@
+
+//ACLARATION
+
 class Superhero {
-    constructor(name, superpower, humilityScore) {
-      this.name = name;
-      this.superpower = superpower;
-      this.humilityScore = humilityScore;
-    }
-    get humilityScore() {
-      return this._humilityScore;
-    }
-  
-    set humilityScore(score) {
-      if (score < 1) {
-        throw new Error("Error: Humility score cannot be less than 1");
-      } else if (score > 10) {
-        throw new Error("Error: Humility score cannot be greater than 10");
-      } else {
-        this._humilityScore = score;
-      }
+  constructor(name, superpower, humilityScore, id) {
+    this.name = name;
+    this.superpower = superpower;
+    this.humilityScore = humilityScore;
+    this.id = id;
+  }
+  get humilityScore() {
+    return this._humilityScore;
+  }
+
+  set humilityScore(score) {
+    if (score < 1) {
+      throw new Error("Error: Humility score cannot be less than 1");
+    } else if (score > 10) {
+      throw new Error("Error: Humility score cannot be greater than 10");
+    } else {
+      this._humilityScore = score;
     }
   }
-  
-  module.exports = Superhero;
-  
+}
+
+module.exports = Superhero;
